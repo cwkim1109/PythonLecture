0448b2a047faf7e05eb555a7b31955d8f6fb4780
Bobby Kim <cwkim1109@gmail.com>
2021-08-03 12:31:36 +0900

    test3

--- Lecture_Python/DemoLoop.py
+++ Lecture_Python/DemoLoop.py
@@ -18,7 +18,6 @@ for i in lst:
         continue
     print("Item:{0}".format(i))
 
-
 # 수열함수
 print( range (10))
 print(list(range(10)))
@@ -26,7 +25,6 @@ print(set(range(10)))
 print(tuple(range(10)))
 print(list(range(2000,2022)))
 print(list(range(10,0,-1)))
-
 
 # 수동으로 루프를 돌리는 경우
 for i in range(5):
